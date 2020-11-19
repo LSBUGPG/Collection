418255e7986e6b30082b3da9a28592c19eeabaaf
Paul Sinnett <paul.sinnett@gmail.com>
2020-11-19 13:42:03 +0000

    added test keys

--- Assets/Collections.cs
+++ Assets/Collections.cs
@@ -4,11 +4,18 @@ using UnityEngine;
 using UnityEngine.UI;
 
 [System.Serializable]
+public class Item
+{
+    public Sprite image;
+    public bool collected;
+}
+
+[System.Serializable]
 public class Collection
 {
     public string title;
     public Sprite image;
-    public List<Sprite> contents;
+    public List<Item> contents;
 }
 
 public class Collections : MonoBehaviour
@@ -21,9 +28,9 @@ public class Collections : MonoBehaviour
         Collection data = collections[collection];
         for (int i = 0; i < 6; i++)
         {
-            if (i < data.contents.Count && data.contents[i] != null)
+            if (i < data.contents.Count && data.contents[i].collected)
             {
-                content.images[i].sprite = data.contents[i];
+                content.images[i].sprite = data.contents[i].image;
                 content.images[i].enabled = true;
             }
             else
@@ -34,5 +41,29 @@ public class Collections : MonoBehaviour
         }
 
         content.gameObject.SetActive(true);
+    }
+
+    public void CloseCollection()
+    {
+        content.gameObject.SetActive(false);
+    }
+
+    public void Collect(string title, int index)
+    {
+        Collection collection = 
+            collections.Find((entry) => entry.title == title);
+
+        if (collection != null && index < collection.contents.Count)
+        {
+            collection.contents[index].collected = true;
+            Debug.LogFormat("{0} {1} collected", title, index);
+        }
+        else
+        {
+            Debug.LogWarningFormat(
+                "Item {0} {1} does not exist",
+                title,
+                index);
+        }
     }
 }
---
+++ ReadMe.md
@@ -0,0 +1,11 @@
+# Collections
+
+- Click on a button to open that collection.
+- Click anywhere on the collection to close it.
+
+## Test code
+
+- Press the 1 key to pick up the first item of the first collection
+- Press the 2 key to pick up the second item of the first collection
+- Press the 3 key to pick up the first item of the second collection
+- Press the 4 key to see the warning message if an item doesn't exist
